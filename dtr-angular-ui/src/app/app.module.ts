f18f4eb85a96f3ccbe03f6c9d6064548e9271dde
Tom Carroll <thomas.carroll67@gmail.com>
2021-07-01 12:54:20 -0500

    Got homepage ui done. Navbar now changes components displayed. Added all components for nav Items. NavBar(click -> navItem id) -> Background(navItem id) -> Nav item component

--- dtr-angular-ui/src/app/app.module.ts
+++ dtr-angular-ui/src/app/app.module.ts
@@ -5,22 +5,31 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { BackgroundComponent } from './background/background.component';
-import { ShowsComponent } from './shows/shows.component';
-import { AppHamburgerToggleDirective } from './app-hamburger-toggle.directive';
-import { HamburgerToggleDirective } from './navbar/hamburger-toggle.directive';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AboutComponent } from './background-items/about/about.component'
+import { MusicComponent } from './background-items/music/music.component'
+import { ShopComponent } from './background-items/shop/shop.component'
+import { ShowsComponent } from './background-items/shows/shows.component'
+import { VideosComponent } from './background-items/videos/videos.component'
+
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     BackgroundComponent,
+    AboutComponent,
+    MusicComponent,
+    ShopComponent,
     ShowsComponent,
-    AppHamburgerToggleDirective,
-    HamburgerToggleDirective
+    VideosComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- dtr-angular-ui/src/app/navbar/navbar.component.ts
+++ dtr-angular-ui/src/app/navbar/navbar.component.ts
@@ -1,18 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnDestroy, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent implements OnInit {
-
-  
+export class NavbarComponent implements OnInit, OnDestroy{
+  public isCollapsed = false;
+  public navItem = 0;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  ngOnDestroy(): void {
+    console.log("destroyed");
+  }
+
+  public showMenu(){
+    this.isCollapsed = true;
+  }
+  
+  public changeBackground(navId: number){
+    this.navItem = navId;
+  }
 
 }
