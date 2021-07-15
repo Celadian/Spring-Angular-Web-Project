aa52502a7fe62d4d336f69811a5cb555bb6a6a56
Tom Carroll <thomas.carroll67@gmail.com>
2021-07-14 20:02:58 -0500

    cleaned up footer

--- dtr-angular-ui/src/app/app.module.ts
+++ dtr-angular-ui/src/app/app.module.ts
@@ -1,9 +1,6 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AboutComponent } from './navigation-items/about/about.component'
 import { MusicComponent } from './navigation-items/music/music.component'
 import { ShopComponent } from './navigation-items/shop/shop.component'
@@ -11,8 +8,16 @@ import { ShowsComponent } from './navigation-items/shows/shows.component'
 import { VideosComponent } from './navigation-items/videos/videos.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './navigation-items/home/home.component';
+
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms'; 
 import {TextFieldModule} from '@angular/cdk/text-field';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
+
 
 
 
@@ -33,11 +38,15 @@ import { MatFormFieldModule } from '@angular/material/form-field';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule,
     TextFieldModule,
-    MatFormFieldModule
+    MatFormFieldModule,
+    MatInputModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+}
